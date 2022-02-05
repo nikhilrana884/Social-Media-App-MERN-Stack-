f369648d6e0720de06644e90de9d967b4f1da85c
nikhilrana884 <nikhilrana884@gmail.com>
2022-02-05 18:54:25 +0530

    User Route tested

--- index.js
+++ index.js
@@ -5,12 +5,21 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const helmet = require("helmet");
 const morgan = require("morgan");
+const userRoute = require("./routes/users")
 
 dotenv.config();
 
 mongoose.connect(process.env.MONGO_URL,{ useNewUrlParser: true}, ()=> {
     console.log("Connected to MongoDB")
 });;
+
+//MIDDLEWARE 
+
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
+
+app.use("/api/users",userRoute);
 
 app.listen(8800,()=>{
     console.log("Backend server is running")
