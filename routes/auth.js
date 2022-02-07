46ce272b7688f9ff8ec5822c6fff7a4aa737769e
nikhilrana884 <nikhilrana884@gmail.com>
2022-02-07 10:36:21 +0530

    user password encrypted in db using bcrypt

--- routes/auth.js
+++ routes/auth.js
@@ -1,19 +1,26 @@
 const router = require("express").Router();
 const User = require("../models/User");
+const bcrypt = require("bcrypt")
 
 //REGISTER
 
 
 router.post("/register",async (req,res)=>{
-    const newUser = new User({
-        username:req.body.username,
-        email:req.body.email,
-        password:req.body.username,
-
-    });
-
+   
     try
     {
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(req.body.password,salt);
+
+        //CREATE USER
+        const newUser = new User({
+            username:req.body.username,
+            email:req.body.email,
+            password:hashedPassword,
+    
+        });
+
+        //SAVE USER AND RESPOND
         const user = await newUser.save();
         res.status(200).json(user);
     }  
