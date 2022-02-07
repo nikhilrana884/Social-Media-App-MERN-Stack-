4b33a6b28d4c5a367c86142d12fe3a543219168b
nikhilrana884 <nikhilrana884@gmail.com>
2022-02-07 10:25:04 +0530

    Testing on Postman

--- routes/auth.js
+++ routes/auth.js
@@ -5,7 +5,25 @@ const User = require("../models/User");
 
 
 router.post("/register",async (req,res)=>{
+    const newUser = new User({
+        username:req.body.username,
+        email:req.body.email,
+        password:req.body.username,
+
+    });
+
+    try
+    {
+        const user = await newUser.save();
+        res.status(200).json(user);
+    }  
+    catch(err)
+    {
+        console.log(err);
+    }
     
+
+
 });
 
 module.exports =  router;
