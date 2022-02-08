f35c06ffc978016830b3154e08b5f063fae6b6a8
nikhilrana884 <nikhilrana884@gmail.com>
2022-02-08 20:45:20 +0530

    User CRUD , follow,unfollow Operations Added

--- routes/users.js
+++ routes/users.js
@@ -89,7 +89,7 @@ router.put("/:id/follow", async (req, res) =>
         } 
         else 
         {
-          res.status(403).json("you already follow this user");
+          res.status(403).json("You already follow this user");
         }
       } 
       catch (err) 
@@ -103,5 +103,38 @@ router.put("/:id/follow", async (req, res) =>
     }
   });
 
+
+
+//UNFOLLOW
+
+router.put("/:id/unfollow", async (req, res) => 
+{
+  if (req.body.userId !== req.params.id) 
+  {
+    try 
+    {
+      const user = await User.findById(req.params.id);
+      const currentUser = await User.findById(req.body.userId);
+      if (user.followers.includes(req.body.userId)) 
+      {
+        await user.updateOne({ $pull: { followers: req.body.userId } });
+        await currentUser.updateOne({ $pull: { followings: req.params.id } });
+        res.status(200).json("User has been unfollowed");
+      } 
+      else 
+      {
+        res.status(403).json("You dont follow this user");
+      }
+    } 
+    catch (err) 
+    {
+      res.status(500).json(err);
+    }
+  } 
+  else 
+  {
+    res.status(403).json("You cant unfollow yourself");
+  }
+});
 
 module.exports =  router
