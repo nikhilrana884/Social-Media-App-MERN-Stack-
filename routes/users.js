56ec83e46c27803b49bf3a28d2dcf9e4e1499179
nikhilrana884 <nikhilrana884@gmail.com>
2022-02-08 12:46:29 +0530

    Delete Operation added for user by id

--- routes/users.js
+++ routes/users.js
@@ -4,39 +4,73 @@ const bcrypt = require("bcrypt");
 //CRUD
 
 //UPDATE
-router.put("/:id",async (req,res)=>{
-    if(req.body.userId===req.params.id || req.user.isAdmin)
+router.put("/:id", async (req, res) => {
+    if (req.body.userId === req.params.id || req.body.isAdmin) 
     {
-        if(req.body.password)
+      if (req.body.password) 
+      {
+        try 
         {
-            try
-            {
-                const salt = await bcrypt.genSalt(10);
-                req.body.password = await bcrpyt.hash(req.body.password,salt);
-            } 
-            catch(err)
-            {
-                return res.status(500).json(err);
-            }
-        }
-        try
-        {
-            const user = await User.findByIdAndUpdate(req.params.id,{
-                $set:req.body,
-            });
-            res.status(200).json("Account Updated");
+          const salt = await bcrypt.genSalt(10);
+          req.body.password = await bcrypt.hash(req.body.password, salt);
         } 
-        catch(err)
+        catch (err)
         {
-            return res.status(500).json(err);
+          return res.status(500).json(err);
         }
-
-    }
-    else
+      }
+      try 
+      {
+        const user = await User.findByIdAndUpdate(req.params.id, {
+          $set: req.body,
+        });
+        res.status(200).json("Account has been updated");
+      } 
+      catch (err) 
+      {
+        return res.status(500).json(err);
+      }
+    } 
+    else 
     {
-        return res.status(403).json(" Account not matching.");
+      return res.status(403).json("You can update only your account!");
     }
-}) 
+  });
 
 
+//DELETE
+router.delete("/:id", async (req, res) => {
+    if (req.body.userId === req.params.id || req.body.isAdmin) 
+    {
+      try 
+      {
+        await User.findByIdAndDelete(req.params.id);
+        res.status(200).json("Account has been deleted");
+      } 
+      catch (err) 
+      {
+        return res.status(500).json(err);
+      }
+    } 
+    else 
+    {
+      return res.status(403).json("You can delete only your account!");
+    }
+  });
+  
+  router.get("/:id", async (req, res) => 
+  {
+    try {
+      const user = await User.findById(req.params.id);
+      const { password, updatedAt, ...other } = user._doc;
+      res.status(200).json(other);
+    } 
+    catch (err) 
+    {
+      res.status(500).json(err);
+    }
+  });
+
+
+  
 module.exports =  router
