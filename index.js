29041b2a722daef3f4367e748151ae740dc67653
nikhilrana884 <nikhilrana884@gmail.com>
2022-02-09 16:27:04 +0530

    posts api created

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const helmet = require("helmet");
 const morgan = require("morgan");
 const userRoute = require("./routes/users")
 const authRoute = require("./routes/auth")
+const postRoute = require("./routes/post")
 
 dotenv.config();
 
@@ -22,6 +23,7 @@ app.use(morgan("common"));
 
 app.use("/api/users",userRoute);
 app.use("/api/auth",authRoute);
+app.use("/api/posts",postRoute);
 
 
 
