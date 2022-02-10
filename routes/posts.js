20dfc8833868e28d930137020002691ac2f7e620
nikhilrana884 <nikhilrana884@gmail.com>
2022-02-10 13:22:38 +0530

    Posts' updation api configured

--- routes/posts.js
+++ routes/posts.js
@@ -19,4 +19,32 @@ router.post("/", async (req, res) => {
 });
 
 
+//UPDATE
+router.put("/:id", async (req, res) => 
+{
+  try 
+  {
+    const post = await Post.findById(req.params.id);
+    if (post.userId === req.body.userId) 
+    {
+      await post.updateOne({ $set: req.body });
+      res.status(200).json("The post has been updated");
+    } 
+    else 
+    {
+      res.status(403).json("You can update only your post");
+    }
+  } 
+  catch (err) 
+  {
+    res.status(500).json(err);
+  }
+});
+
+
+
+
+
+
+
 module.exports = router;
